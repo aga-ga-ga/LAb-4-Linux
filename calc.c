4a6328449573c1f4cf3ad51f54c713e1855a389d
aga-ga-ga <sasha_asp@list.ru>
2014-05-26 14:32:38 +0300

    Make lab 5 (CalcSYS) in lab 4
    Try to add CalcSYS in lab 4.

--- calc.c
+++ calc.c
@@ -1,7 +1,6 @@
 #include <linux/module.h>
 #include <linux/kernel.h>
 #include <linux/string.h>
-#include <linux/proc_fs.h>
 
 #define WRITE_SIZE 100
 
@@ -12,58 +11,6 @@
 
 #define PARENT_DIR "calc"
 
-struct proc_dir_entry *calc_dir;
-struct proc_dir_entry *arg1;
-struct proc_dir_entry *arg2;
-struct proc_dir_entry *operation;
-struct proc_dir_entry *result;
-
-
-static char arg1_input[WRITE_SIZE];
-static char arg2_input[WRITE_SIZE];
-static char operation_input[WRITE_SIZE];
-
-/*
- * arg1 write handler
- */
-int write_arg1(struct file *file, const char *buf, unsigned long count, void *data)
-{
-	if(count > WRITE_SIZE) {
-    	count = WRITE_SIZE;
-	}
-	memcpy(arg1_input, buf, count);
-	return count;
-}
-
-/*
- * arg2 write handler
- */
-int write_arg2(struct file *file, const char *buf, unsigned long count, void *data)
-{
-	if(count > WRITE_SIZE) {
-    	count = WRITE_SIZE;
-	}
-	memcpy(arg2_input, buf, count);
-	return count;
-}
-
-/*
- * operation write handler
- */
-int write_operation(struct file *file, const char *buf, unsigned long count, void *data)
-{
-	if(count > WRITE_SIZE) {
-    	count = WRITE_SIZE;
-	}
-
-	//memset(operation, 0, WRITE_SIZE);
-	memcpy(operation_input, buf, count);
-	return count;
-}
-
-/*
- * result read handler
- */
 int read_result(char *buffer, char **buffer_location,
                   off_t offset, int buffer_length, int *eof, void *data)
 {
@@ -91,6 +38,176 @@ int read_result(char *buffer, char **buffer_location,
 	return sprintf(buffer, "%ld\n", res);
 }
 
+static char arg1_input[WRITE_SIZE];
+static char arg2_input[WRITE_SIZE];
+static char operation_input[WRITE_SIZE];
+
+
+
+#ifdef SYSFS
+
+#include <linux/init.h>
+#include <linux/fs.h>
+#include <linux/slab.h>
+
+static struct attribute arg1 = {
+	.name = ARG1,
+	.mode = 0666,
+};
+
+static struct attribute arg2 = {
+	.name = ARG2,
+	.mode = 0666,
+};
+
+static struct attribute operation = {
+	.name = OPERATION,
+	.mode = 0666,
+};
+
+static struct attribute result = {
+	.name = RESULT,
+	.mode = 0666,
+};
+
+static struct attribute * calc_attributes[] = {
+	&arg1,
+	&arg2,
+	&operation,
+	&result,
+	NULL
+};
+
+
+static ssize_t default_show(struct kobject *kobj, struct attribute *attr,
+		char *buf)
+{
+	if (!strcmp(attr->name, RESULT)) {
+		long res = calculate();
+
+		return sprintf(buf, "%ld\n", res);
+	} else {
+		return 0;
+	}
+}
+
+static ssize_t default_store(struct kobject *kobj, struct attribute *attr,
+		const char *buf, size_t len)
+{
+	if(len > WRITE_SIZE) {
+		len = WRITE_SIZE;
+	}
+
+	if (!strcmp(attr->name, ARG1)) {
+		memcpy(arg1_input, buf, len);
+	} else if (!strcmp(attr->name, ARG2)) {
+		memcpy(arg2_input, buf, len);
+	} else if (!strcmp(attr->name, OPERATION)) {
+		memcpy(operation_input, buf, len);
+	}
+	return len;
+}
+
+static struct sysfs_ops calc_ops = {
+	.show = default_show,
+	.store = default_store,
+};
+
+static struct kobj_type calc_type = {
+	.sysfs_ops = &calc_ops,
+	.default_attrs = calc_attributes,
+};
+//Во внешнем представлении в каталоге /sys каждому объекту struct kobject соответствует каталог, что видно и из самого определения структуры
+//Но это вовсе не означает, что каждый инициализированный объект struct kobject автоматически экспортируется в файловую систему /sys. 
+//Для того, чтобы сделать объект видимым в /sys, необходимо вызвать: kobject_add
+struct kobject *calc_obj;
+static int __init sysfsexample_module_init(void)
+{
+	int err = -1;
+	//GFP_KERNE  Это обычный запрос, который может блокировать поток. Данный флаг используется при выделении памяти в контексте потока, который может заснуть.
+	calc_obj = kzalloc(sizeof(*calc_obj), GFP_KERNEL);
+	if (calc_obj) {
+		kobject_init(calc_obj, &calc_type);
+		if (kobject_add(calc_obj, NULL, "%s", PARENT_DIR)) {
+			 err = -1;
+			 printk("Sysfs creation failed\n");
+			 //вызов kobject_put () будет уменьшать 
+			 //счетчик ссылок и, возможно, освободить объект calc_obj
+			 kobject_put(calc_obj);
+			 calc_obj = NULL;
+		}
+		err = 0;
+	}
+	return err;
+}
+
+static void __exit sysfsexample_module_exit(void)
+{
+	if (calc_obj) {
+		kobject_put(calc_obj);
+		kfree(calc_obj);
+	}
+}
+
+module_init(sysfsexample_module_init);
+module_exit(sysfsexample_module_exit);
+MODULE_LICENSE("GPL");
+
+
+#else
+
+#include <linux/proc_fs.h>
+
+struct proc_dir_entry *calc_dir;
+struct proc_dir_entry *arg1;
+struct proc_dir_entry *arg2;
+struct proc_dir_entry *operation;
+struct proc_dir_entry *result;
+
+
+/*
+ * arg1 write handler
+ */
+int write_arg1(struct file *file, const char *buf, unsigned long count, void *data)
+{
+	if(count > WRITE_SIZE) {
+    		count = WRITE_SIZE;
+	}
+	memcpy(arg1_input, buf, count);
+	return count;
+}
+
+/*
+ * arg2 write handler
+ */
+int write_arg2(struct file *file, const char *buf, unsigned long count, void *data)
+{
+	if(count > WRITE_SIZE) {
+    		count = WRITE_SIZE;
+	}
+	memcpy(arg2_input, buf, count);
+	return count;
+}
+
+/*
+ * operation write handler
+ */
+int write_operation(struct file *file, const char *buf, unsigned long count, void *data)
+{
+	if(count > WRITE_SIZE) {
+    		count = WRITE_SIZE;
+	}
+
+	//memset(operation, 0, WRITE_SIZE);
+	memcpy(operation_input, buf, count);
+	return count;
+}
+
+/*
+ * result read handler
+ */
+
+
 int init_module()
 {
 	// parent dir
@@ -107,8 +224,8 @@ int init_module()
 	// С помощью команды create_proc_entry() в указанной выше поддиректории создается обычный файл "ARG1" с правами доступа 0666.
 	arg1 = create_proc_entry(ARG1, 0666, calc_dir);
 	if(!arg1) {
-    	printk(KERN_INFO "Error creating proc entry");
-    	return -ENOMEM;
+	    	printk(KERN_INFO "Error creating proc entry");
+	    	return -ENOMEM;
     	}
 	//запись данных в виртуальный файл (из пользовательского пространства в ядро)
 	arg1->write_proc = write_arg1;
@@ -116,24 +233,24 @@ int init_module()
 	// arg2
 	arg2 = create_proc_entry(ARG2, 0666, calc_dir);
 	if(!arg2) {
-    	printk(KERN_INFO "Error creating proc entry");
-    	return -ENOMEM;
+	    	printk(KERN_INFO "Error creating proc entry");
+	    	return -ENOMEM;
     	}
 	arg2->write_proc = write_arg2;
 
 	// operation
 	operation = create_proc_entry(OPERATION, 0666, calc_dir);
 	if(!operation) {
-    	printk(KERN_INFO "Error creating proc entry");
-    	return -ENOMEM;
+	    	printk(KERN_INFO "Error creating proc entry");
+	    	return -ENOMEM;
     	}
 	operation->write_proc = write_operation;
 
 	// result
 	result = create_proc_entry(RESULT, 0666, calc_dir);
 	if(!result) {
-    	printk(KERN_INFO "Error creating proc entry");
-    	return -ENOMEM;
+	    	printk(KERN_INFO "Error creating proc entry");
+	    	return -ENOMEM;
 	}
 	//считывает данные из виртуального файла (из ядра в пользовательское пространство) 
 	result->read_proc = read_result;
@@ -150,4 +267,4 @@ void cleanup_module()
 	remove_proc_entry(RESULT, NULL);
 	printk(KERN_INFO "/proc/%s removed\n", PARENT_DIR);
 }
-
+#endif
